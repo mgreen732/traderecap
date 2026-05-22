7a40d4428872db794edcea9d0b27721136247fd9
Mgreen732 <melvingreen1031@gmail.com>
2026-05-21 22:55:59 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,4 @@
-
+Tap "Add file" → "Create new file"
+In the filename box type src/main.jsx — GitHub automatically creates the folder when you put a / in the name
+Copy the contents of main.jsx from above and paste it in
+Tap "Commit changes"
